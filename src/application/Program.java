e6affcd164ee8fe0d0d80ff951b31873dd3c6660
Henrique <henriqueflavioguimaraes@gmail.com>
2026-05-06 18:44:25 -0300

    feat: Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		
 		Department obj = new Department(1, "Tecnologia");
-		System.out.println(obj);
 		
+		Seller seller = new Seller(2, "Bob", "bob@gmail.com", new Date(), 2000.00, obj);
+		System.out.println(seller);
 	}
 
 }
