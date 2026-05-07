49d202f8b74867fcb844e6570ea9259f1ee7e945
Henrique <henriqueflavioguimaraes@gmail.com>
2026-05-07 18:03:25 -0300

    feat: SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Date;
 import java.util.Locale;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,9 @@ public class Program {
 		Department obj = new Department(1, "Tecnologia");
 		
 		Seller seller = new Seller(2, "Bob", "bob@gmail.com", new Date(), 2000.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 
